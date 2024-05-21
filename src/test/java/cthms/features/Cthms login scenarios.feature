9b657e1d95e818cf47322747291da63c449a93d3
OlyaBuhrym <olhabuhrym@gmail.com>
2024-05-21 11:12:14 +0100

    Olya

---
+++ src/test/java/cthms/features/Cthms login scenarios.feature
@@ -0,0 +1,3 @@
+Feature:
+  Scenario: CTHMS Login with valid credentials
+    Given a CTHMS user is on the Login Page
---
+++ src/test/java/cthms/runners/RunCTHMSProgressionTest.java
@@ -0,0 +1,15 @@
+package cthms.runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/CTHMS/features",
+        glue = "CTHMS.steps",
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        dryRun = false
+)
+
+public class RunCTHMSProgressionTest extends AbstractTestNGCucumberTests {
+}
